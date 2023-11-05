9e41fff3f141ed5fe5d2497bb7cc42695ded8f9e
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-05 21:26:31 +0600

    Modify navbar to show logged in user name and img

--- src/Layout/Shared/Navbar/Navbar.jsx
+++ src/Layout/Shared/Navbar/Navbar.jsx
@@ -1,17 +1,44 @@
-import { NavLink } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { useContext } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 
 const Navbar = () => {
 
+    const { user, signOutUser } = useContext(AuthContext);
+
     const navLinks = <>
-        <li> <NavLink to="/"><a>Home</a> </NavLink></li>
-        <li> <NavLink to="/assignment-create"><a>Assignment Create</a> </NavLink></li>
-        <li> <NavLink to="/login"><a>Login</a> </NavLink></li>
-        <li> <NavLink to="/register"><a>Register</a> </NavLink></li>
+        <li> <NavLink to="/">Home</NavLink></li>
+        <li> <NavLink to="/assignment-create">Assignment Create</NavLink></li>
+        {
+            !user &&
+            <>
+
+                <li> <NavLink to="/login">Login</NavLink> </li>
+                <li> <NavLink to="/register">Register</NavLink> </li>
+            </>
+        }
+
     </>;
+
+    const handleClickLogout = () => {
+        signOutUser()
+            .then(() => {
+                toast.success(" Sign-out successful");
+            })
+            .catch(error => {
+                toast.error(error);
+            });
+    }
 
     return (
         <div className="navbar bg-base-100">
+
+            <Toaster
+                position="top-right"
+                reverseOrder={false}
+            />
+
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -29,7 +56,44 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Login</a>
+
+                {!user && <Link to="/login" className="btn">Login</Link>}
+                {
+                    user &&
+                    <>
+                        <div className="flex gap-1 justify-center items-center">
+                            <div className="avatar m-2">
+                                <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img src={user.photoURL} />
+                                </div>
+                            </div>
+                            <div className="md:hidden">
+                                <button onClick={handleClickLogout}
+                                    className="px-3 py-1 rounded-md text-sm font-medium border focus:outline-none focus:ring transition text-red-600 border-red-600 hover:text-white hover:bg-red-600 active:bg-red-700 focus:ring-red-300" >
+                                    Logout
+                                </button>
+                            </div>
+
+                            <div className="hidden md:flex flex-col items-center">
+                                <div>
+                                    <p className="font-bold"> Hello, <span className="text-sm font-semibold text-gray-500">{user.displayName}</span> </p>
+                                </div>
+                                {/* <div>
+                                    <p className="text-sm font-semibold text-gray-700">{user.email}</p>
+                                </div> */}
+
+                                <div>
+                                    <button onClick={handleClickLogout}
+                                        className="px-3 py-1 rounded-md text-sm font-medium border focus:outline-none focus:ring transition text-red-600 border-red-600 hover:text-white hover:bg-red-600 active:bg-red-700 focus:ring-red-300" >
+                                        Logout
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </>
+                }
+
             </div>
         </div>
     );
