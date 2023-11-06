88ff3d58c0fe8a7f8496d254518ffb36032c2fb8
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-07 00:13:05 +0600

    Add feature to prevent a user updating assignments created by other users

--- src/pages/AllAssignment/AssignmentCard.jsx
+++ src/pages/AllAssignment/AssignmentCard.jsx
@@ -1,9 +1,31 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from 'sweetalert2'
 
 const AssignmentCard = ({ assignment }) => {
 
-    const { _id, email, title, difficultyLevel, dueDate, thumbnail, marks, description } = assignment;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const loggedInEmail = user.email;
+
+    const { _id, email, title, difficultyLevel, dueDate, thumbnail, marks } = assignment;
+
+    const itemCreatorEmail = email;
+
+    const handleView = () => {
+        if (loggedInEmail !== itemCreatorEmail) {
+            Swal.fire({
+                icon: "error",
+                title: "Can't update other user's Item",
+                text: `Created by user: ${email}`,
+            });
+        } else {
+            navigate(`/assignment-update/${_id}`);
+        }
+    }
 
     return (
         <div className="card w-72 shadow-lg dark:border">
@@ -21,9 +43,7 @@ const AssignmentCard = ({ assignment }) => {
                         <Link to={`/view-assignment/${_id}`}>
                             <button className="btn btn-primary normal-case">View</button>
                         </Link>
-                        <Link to={`/assignment-update/${_id}`}>
-                            <button className="btn btn-secondary normal-case">Update</button>
-                        </Link>
+                        <button onClick={handleView} className="btn btn-secondary normal-case">Update</button>
                     </div>
                 </div>
             </div>
@@ -32,3 +52,7 @@ const AssignmentCard = ({ assignment }) => {
 };
 
 export default AssignmentCard;
+
+AssignmentCard.propTypes = {
+    assignment: PropTypes.object
+}
