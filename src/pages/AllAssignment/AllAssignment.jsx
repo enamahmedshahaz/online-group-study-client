235ab3248906dc6269d4610f6d924cefdf983153
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-09 13:03:43 +0600

    Add pagination feature to All assignments page

--- src/pages/AllAssignment/AllAssignment.jsx
+++ src/pages/AllAssignment/AllAssignment.jsx
@@ -6,44 +6,65 @@ import AssignmentCard from "./AssignmentCard";
 const AllAssignment = () => {
 
     const [assignments, setAssignments] = useState([]);
+
     const [level, setLevel] = useState("easy");
-
-    useEffect(() => {
-        axios
-            .get(`http://localhost:5000/assignments?level=${level}`)
-            .then(res => setAssignments(res.data))
-            .catch(err => console.log(err));
-    }, [level]);
-
-
+    
     const handleChangeLevel = (e) => {
         const filterValue = e.target.value;
         console.log(filterValue);
         setLevel(filterValue);
     }
 
-    //----------pagination related code-------/
-
-    const assignmentCount = assignments.length;
+    const [assignmentCount, setAssignmentCount] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
 
     const itemsPerPage = 8;
 
-    //const [itemsPerPage, setItemsPerPage] = useState(10);
-
     const numberOfPages = Math.ceil(assignmentCount / itemsPerPage);
-
-    const [currentPage, setCurrentPage] = useState(0);
 
     const pages = [...Array(numberOfPages).keys()];
 
+    //count assignments based on level
+    useEffect(() => {
+        const url = `http://localhost:5000/assignmentCount?level=${level}`;
+        axios
+            .get(url)
+            .then(res => {
+                setAssignmentCount(parseInt(res.data.count));
+                setCurrentPage(0);
+            })
+            .catch(err => console.log(err));
+    }, [level]);
 
-    //----------END of pagination related code-------/
+    //load assignments based on query params level, currentPage, size 
+    useEffect(() => {
+        const url = `http://localhost:5000/assignments?level=${level}&page=${currentPage}&size=${itemsPerPage}`;
+        console.log(url);
+        axios
+            .get(url)
+            .then(res => setAssignments(res.data))
+            .catch(err => console.log(err));
+    }, [level, currentPage, itemsPerPage]);
+
+
+
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
 
     return (
         <div>
 
             <div className="py-4 text-center mb-5 ">
-                <h2 className="font-medium text-6xl text-gray-600">Assignments ({assignments.length})</h2>
+                <h2 className="font-medium text-6xl text-gray-600">Assignments ({assignmentCount})</h2>
 
                 <div className="form-control">
                     <label className="label">
@@ -77,24 +98,17 @@ const AllAssignment = () => {
 
             {/* pagination */}
             <div className="text-center">
-                <div className="join my-5 ">
+                <div className="my-5">
 
-
+                <button className="btn mr-5" onClick={handlePrevPage}>Prev</button>
                     {
-
                         //    pages.map(page => <button className={currentPage === page ? 'selected' : undefined} onClick={() => setCurrentPage(page)} key={page}>{page}</button>)
-                        pages.map(page => <button className="join-item btn" onClick={() => setCurrentPage(page)} key={page}>{page}</button>)
-
+                        pages.map(page => <button className={`mr-5 btn ` + (currentPage === page ? 'bg-red-500' : '')} onClick={() => setCurrentPage(page)} key={page}>{page + 1}</button>)
                     }
+                <button className="btn" onClick={handleNextPage}>Next</button>
 
-                    <button className="join-item btn">1</button>
-                    <button className="join-item btn btn-active">2</button>
-                    <button className="join-item btn">3</button>
-                    <button className="join-item btn">4</button>
                 </div>
             </div>
-
-
 
         </div>
     );
